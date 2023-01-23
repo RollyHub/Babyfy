6d29fc92d97c48533f940b47aedf716613a31f09
rollyHub <rolly_farias@hotmail.com>
2023-01-23 14:17:34 +0100

    added subtotal kod

---
+++ src/Subtotal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./Subtotal.css";
+import CurrencyFormat from 'react-currency-format';
+import { faBasketball } from '@fortawesome/free-solid-svg-icons';
+
+function Subtotal() {
+  return <div className="subtotal">
+    <CurrencyFormat
+      renderText={(value) => (
+        <>
+          <p>
+            Subtotal (0 items): 
+            <strong>0</strong>
+          </p>
+          <small className="subtotal__gift">
+            <input type="checkbox" /> This order
+            contains a gift
+          </small>
+        </>
+      )}
+      decimalScale={2}
+      value={getBasketTotal(basket)}
+      displayType={"text"}
+      thousandSeparator={true}
+      prefix={"€"}
+      />
+    </div>;
+  
+}
+
+export default Subtotal
